23387cc05a3ff4b079acfb38015a3bf511413968
orIsc <orjslash27@gmail.com>
2020-12-28 19:16:09 +0200

    first push

---
+++ flaskr/admin.py
@@ -0,0 +1,22 @@
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, url_for
+)
+from werkzeug.exceptions import abort
+
+from flaskr.auth import login_required
+from flaskr.db import get_db
+
+bp = Blueprint('admin', __name__)
+
+
+@bp.route('/admin')
+def admin():
+    db = get_db()
+    posts = db.execute(
+        'SELECT p.id, title, body, created, author_id, username'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' ORDER BY created DESC'
+    ).fetchall()
+    return render_template('admin/admin.html', posts=posts)
+
+
